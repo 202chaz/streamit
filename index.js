9d21a60811e709addaadc164c142a333c4f25e48
Chaz Haskins <chaz.haskins@gmail.com>
2025-02-05 12:31:44 -0500

    Initial Commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const NodeMediaServer = require("node-media-server");
+
+const httpConfig = {
+  port: 8000,
+  allow_origin: "*",
+  mediaroot: "./media",
+};
+
+const rtmpConfig = {
+  port: 1935,
+  chunk_size: 60000,
+  gop_cache: true,
+  ping: 30,
+  ping_timeout: 60,
+};
+
+const config = {
+  http: httpConfig,
+  rtmp: rtmpConfig,
+};
+
+const nms = new NodeMediaServer(config);
+
+// Use run method to start our media server.
+nms.run();
